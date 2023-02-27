f9c71fea6ff19eb905f8a1f629af11cf64cec3fa
CloneCharizard <clonecharizard@hotmail.co.jp>
2023-02-27 10:23:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,24 +12,12 @@ app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json())
 
 import gogoRoutes from './Routes/Gogoanime.js';
-import animixRoutes from './Routes/Animixplay.js';
-import zoroRoutes from './Routes/Zoro.js';
-import allanimeRoutes from './Routes/Allanime.js';
-import animepaheRoutes from './Routes/Animepahe.js';
-import yugenRoutes from './Routes/Yugen.js';
-import nineanimeRoutes from './Routes/9anime.js';
 
 app.use('/gogoanime', gogoRoutes);
-app.use('/animix', animixRoutes);
-app.use('/zoro', zoroRoutes);
-app.use('/allanime', allanimeRoutes);
-app.use('/animepahe', animepaheRoutes);
-app.use('/yugen', yugenRoutes);
-app.use(['/nineanime', '/9anime'], nineanimeRoutes);
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Welcome to AnimeAPI!')
+    res.send('Welcome to Toxic Frog API!')
 });
 
 app.get('/schedule', async (req, res) => {
